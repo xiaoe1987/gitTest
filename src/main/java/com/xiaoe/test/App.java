c8b3266f7722e3441d4176e6e7c1bb0114d2c0dc
lei.xu <lei.6.xu@continental-corporation.com>
2020-04-16 13:29:16 +0800

    project init

---
+++ src/main/java/com/xiaoe/test/App.java
@@ -0,0 +1,13 @@
+package com.xiaoe.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
